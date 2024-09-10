a5d23aca151868fd240a4b64d176d1d64768f5de
ahmedkachwaa <kachwaa@zimozi.co>
2024-09-10 20:33:31 +0300

    login screen added

---
+++ app/src/main/java/com/example/github_repo/LoginScreen.kt
@@ -0,0 +1,4 @@
+package com.example.github_repo
+
+class LoginScreen {
+}
--- app/src/main/java/com/example/github_repo/MainActivity.kt
+++ app/src/main/java/com/example/github_repo/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Github_repoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Login",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
